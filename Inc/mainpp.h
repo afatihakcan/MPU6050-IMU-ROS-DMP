20d9bb43a848a174189ddb9734484794978342b8
afatihakcan <70918087+afatihakcan@users.noreply.github.com>
2022-01-05 00:39:01 +0300

    Add files via upload

---
+++ Inc/mainpp.h
@@ -0,0 +1,22 @@
+/*
+ * mainpp.h
+ *
+ *  Created on: Jan 12, 2021
+ *      Author: Fatih
+ */
+
+#ifndef MAINPP_H_
+#define MAINPP_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+void setup();
+void loop();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAINPP_H_ */
